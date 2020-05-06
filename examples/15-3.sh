442c7b9f38dc06aa33bc3aa88348d210eb67f91c
kate123wong <18810809501@163.com>
2020-05-05 22:23:47 -0400

    add 15-3.sh

--- examples/15-3.sh
+++ examples/15-3.sh
@@ -1,7 +1,8 @@
+#!usr/bin/env bash
 # 利用I/O重定向机制
 
 # 清空一个文件（文件大小变为0）
-> file
+#> file
 # 用一段文本内容覆盖一个文件
 echo "some string" > file
 # 在文件尾部追加内容
@@ -16,7 +17,7 @@ read -r line < file
 IFS= read -r line < file
 # 利用外部程序head
 line=$(head -1 file)
-line=`head -1 file`
+#line=`head -1 file`
 
 # 构造一个「畸形」测试用例
 echo -n -e " 123 \x0a456" > file
